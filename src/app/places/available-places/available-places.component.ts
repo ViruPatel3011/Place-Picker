0d25f9b744bee105bdc14ebc6a38c78f45bd69f1
Viral Patel <pviral3011@gmail.com>
2024-10-21 18:01:34 +0530

    Loading state and HTTP errors

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -14,20 +15,42 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
-
+  isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   // constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
+    this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{
         places: Place[];
       }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places.Please try again later.'
+              )
+          );
+        })
+      )
       .subscribe({
-        next: (resData) => {
-          console.log(resData.places);
+        next: (places) => {
+          this.places.set(places);
+        },
+        // err is an object which contains message
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        // When the request is done
+        complete: () => {
+          this.isFetching.set(false);
         },
       });
     this.destroyRef.onDestroy(() => {
